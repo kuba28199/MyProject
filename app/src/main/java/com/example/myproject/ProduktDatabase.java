95f8a6d84910ad8aed078f31da1cf416a75cb6d7
Wioletta <violetta.rushnitskaya@gmail.com>
2022-01-24 16:10:59 +0100

    ListaProduktów
    Wyświetlenie się listy produktów.

--- app/src/main/java/com/example/myproject/ProduktDatabase.java
+++ app/src/main/java/com/example/myproject/ProduktDatabase.java
@@ -2,9 +2,11 @@ package com.example.myproject;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,12 +23,29 @@ public abstract class ProduktDatabase extends RoomDatabase {
         if (INSTANCE == null) {
             synchronized (ProduktDatabase.class) {
                 if(INSTANCE == null){
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ProduktDatabase.class, "produkt_db").build();
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ProduktDatabase.class, "produkt_db").addCallback(sRoomDatabaseCallback).build();
                 }
             }
         }
         return INSTANCE;
     }
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            databaseWriteExecutor.execute(()-> {
+                ProduktDao dao = INSTANCE.produktDao();
+                dao.deleteAll();
+
+                Produkt produkt = new Produkt("Hamburger", 5, "Plaster wołowiny, pikle, cebula, keczup i musztarda!");
+                dao.insert(produkt);
+                Produkt produkt1 = new Produkt("Chicken Burger", 8, "Kotlet z piersi kurczaka w złocistej panierce, sos kanapkowy oraz pokrojone liście sałaty!");
+                dao.insert(produkt1);
+                Produkt produkt2 = new Produkt("Cheeseburger", 8, "");
+                dao.insert(produkt2);
+            });
+        }
+    };
 
 }
